72b6be81ff3318e4708ac449fb022341202b548b
Julian Vennen <julian@aternos.org>
2025-09-10 11:23:46 +0200

    Fix BanList class name

--- src/player-list/BanList.ts
+++ src/player-list/BanList.ts
@@ -2,7 +2,7 @@ import PlayerList from "./PlayerList.js";
 import {UserBan} from "../schemas/ban.js";
 import {Player} from "../schemas/player.js";
 
-export default class IPBanList extends PlayerList<UserBan, UserBan, Player> {
+export default class BanList extends PlayerList<UserBan, UserBan, Player> {
     protected getName(): string {
         return 'minecraft:bans';
     }
